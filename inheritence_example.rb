c0e7d64c51fc46399dce68ccc1e5ea86754aee1a
Avraham Luban <lubanavs@gmail.com>
2017-05-02 19:39:51 -0400

    Added super and more attributes

--- inheritence_example.rb
+++ inheritence_example.rb
@@ -20,12 +20,31 @@ end
 
 class Car < Vehicle
 
+  attr_reader :fuel, :make, :model
+  attr_writer :fuel
+
+  def initialize
+    super
+    @fuel = 250
+    @make = "toyota"
+    @model = "camry"
+
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
+
 end
 
+
 class Bike < Vehicle
+
+  def initialize
+    super
+    @make = "mountain bike"
+    @model = "sports gear"
+  end
 
   def ring_bell
     puts "Ring ring!"
@@ -35,7 +54,8 @@ end
 
 bike = Bike.new
 car = Car.new
-p car.accelerate
-p bike.accelerate
+p car.fuel
+car.fuel = 200
+p car.fuel
+p car.make
 car.honk_horn
-bike.ring_bell
