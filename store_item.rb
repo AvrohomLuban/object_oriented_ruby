11bcfb9dab11751da990980f1bea3240125f6f32
Avraham Luban <lubanavs@gmail.com>
2017-05-02 19:56:28 -0400

    fixed store

--- store_item.rb
+++ store_item.rb
@@ -1,19 +1,37 @@
-class Employee
-  attr_reader :first_name, :last_name, :salary
-  attr_accessor :active
-  def initialize(input_hash)
-    @first_name = input_hash[:first_name]
-    @last_name = input_hash[:last_name]
-    @salary = input_hash[:salary]
-    @active = input_hash[:active]
-  end
-  def print_info
-    puts "#{@first_name} #{@last_name} makes $#{@salary} a year."
-  end
-  def give_annual_raise
-    @salary = @salary * 1.05
-  end
+class Gummy
+
+ attr_reader :color, :price, :flavour, :size
+ #attr_writer :price
+ attr_accessor :price
+
+ def initialize(input_hash)
+   @color = input_hash[:color]
+   @price = input_hash[:price]
+   @flavour = input_hash[:flavour]
+   @size = input_hash[:size]
+ end
+
+ def price=(new_price)
+   @price = new_price
+ end
 end
-Add Comment Collapse
+gummy = Gummy.new({color: "Red", price: 3.99, flavour: "cherry", size: "small"})
+
+class Food < Gummy
+
+  attr_reader :shelf_life
+
+  def initialize(input_hash)
+    super
+    @shelf_life = input_hash[:shelf_life]
+  end
+
+end
+
+food = Food.new({color: "Red", price: 3.99, flavour: "cherry", size: "large", shelf_life: 20})
+gummy = Gummy.new(color: "purple", price: 1, flavour: "grape", size: "s")
+p food
+p food.shelf_life
+
 
 
