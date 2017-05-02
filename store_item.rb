99048a1e3eb3cc053f06a7fae3bf07484f0bb795
Avraham Luban <lubanavs@gmail.com>
2017-05-01 20:01:04 -0400

    hashes

---
+++ store_item.rb
@@ -0,0 +1,13 @@
+chocolate = {brand: "hersheys", model: "white", price: 4.99}
+candy = {brand: "jolly ranchers", model: "Awesome tawesome", price: 3.99}
+chips = {brand: "lays", model: "bbq", price: 1.99}
+
+puts "we are selling #{chocolate[:brand]} #{chocolate[:model]} for only #{chocolate[:price]}"
+puts "we are selling #{candy[:brand]} #{candy[:model]} for only #{candy[:price]}"
+puts "we are selling #{chips[:brand]} #{chips[:model]} for only #{chips[:price]}"
+
+chocolate = {:brand => "hersheys", :model => "white", :price => 4.99}
+candy = {:brand => "hersheys", :model => "white", :price => 3.99}
+chips = {:brand => "hersheys", :model => "white", :price => 1.99}
+
+
